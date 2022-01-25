4046e54a6bae507f0d590684f797b57b645257d1
Marco Baresi <marcobaresi@Marcos-MacBook-Air.local>
2022-01-25 12:37:52 +0100

    ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,72 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(CarsApp());
+
+class CarsApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cars App',
+      home: Scaffold(
+        body: SafeArea(
+          child: ListView.builder(
+            // 15 items, per ogni item invoca itemBuilder
+            itemCount: 15,
+            itemBuilder: (context, index) {
+              return _buildItem(index);
+            }
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItem(index) {
+    return ListTile(
+      leading: Icon(Icons.book),
+      title: Text(
+        'Title $index',
+        style: TextStyle(
+          fontSize: 30,
+        ),
+      ),
+      subtitle: Text(
+        'Subtitle $index',
+        style: TextStyle(
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import 'package:flutter/material.dart';
+
 void main() => runApp(IntroductionApp());
 
 class IntroductionApp extends StatelessWidget {
@@ -23,69 +90,103 @@ class IntroductionApp extends StatelessWidget {
             ),
           ),
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Text(
-                  'Sono nel Body',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  'Sono il secondo Text',
-                  style: TextStyle(
-                    color: Colors.black38,
-                    fontSize: 20,
-                    fontStyle: FontStyle.italic,
-                  ),
-                ),
-                Text(
-                  'Sono il terzo Text',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 15,
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Sono nel Body',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  'Sono il secondo Text',
-                  style: TextStyle(
-                    color: Colors.black38,
-                    fontSize: 20,
-                    fontStyle: FontStyle.italic,
-                  ),
-                ),
-                Text(
-                  'Sono il terzo Text',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 15,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+        body: MyListView(),
       ),
     );
   }
 }
+
+class MyBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Text(
+              'Sono nel Body',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Sono il secondo Text',
+              style: TextStyle(
+                color: Colors.black38,
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Text(
+              'Sono il terzo Text',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 15,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Sono nel Body',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Sono il secondo Text',
+              style: TextStyle(
+                color: Colors.black38,
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Text(
+              'Sono il terzo Text',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 15,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+}
+
+class MyListView extends StatelessWidget {
+  final _itemCount = 15;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: _itemCount,
+      itemBuilder: (context, int index) {
+        return buildCard('Text n. $index');
+      }
+    );
+  }
+
+  Widget buildCard(String text) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 10),
+        child: Text(text),
+      ),
+    );
+  }
+
+}
+ */
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ripasso/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(IntroductionApp());
+    await tester.pumpWidget(CarsApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
