0585baa3dbe14072866b1f19d5ef1e3e4c16ef7e
Bares <m.baresi018@studenti.unibs.it>
2022-03-26 19:29:18 +0100

    README updated

---
+++ lib/main.dart
@@ -0,0 +1,148 @@
+// required to use "standard" Widget (MaterialApp, Scaffold, Text, ...)
+import 'package:flutter/material.dart';
+import 'home_stateful.dart';
+
+// required to launch the App
+void main() => runApp(IntroductionApp());
+
+// Widget that contains every widget of My App
+class IntroductionApp extends StatelessWidget {
+  Color c = Colors.blue;
+
+  // required to "build" my application - entry point for composing together
+  // other widgets to make a new widget
+  @override
+  Widget build(BuildContext context) {
+
+    // predefined Widget in Flutter
+    return MaterialApp(
+      title: 'Title',
+
+      // Scaffold provides the high-leve structure for a screen (such as colors)
+      home: Scaffold(
+        backgroundColor: c,
+        appBar: AppBar(
+          backgroundColor: Colors.white38,
+          title: Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              'TITLE',
+              style: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        body: HomeStateful(),
+      ),
+    );
+  }
+}
+
+// custom Widget not used - usable instead of "MyListView()" - row 34
+class MyBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Sono nel Body',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Sono il secondo Text',
+              style: TextStyle(
+                color: Colors.black38,
+                fontSize: 15,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Text(
+              'Sono il terzo Text',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 15,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Sono nel Body',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Sono il secondo Text',
+              style: TextStyle(
+                color: Colors.black38,
+                fontSize: 15,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Text(
+              'Sono il terzo Text',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 15,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+}
+
+// custom Widget used as body of Scaffold - row 34
+class MyListView extends StatelessWidget {
+  final _itemCount = 15;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: _itemCount,
+      itemBuilder: (context, int index) {
+        return buildCard('Text n. $index');
+      }
+    );
+  }
+
+  Widget buildCard(String text) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: Container(
+        child: Row(
+          children: [
+            Expanded(
+              child:Image.asset("assets/minny.jpeg"),
+              flex: 3,
+            ),
+            Expanded(
+              child: Center(child: Text(text)),
+              flex: 1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
